ed3d8b16a8aabda7a7daebab051b0712eb392f66
iktaniyalol <ilonellochka7@gmail.com>
2024-03-16 18:50:21 +1000

    Домашняя работа 4. Добавлен ассет-аватарка и проект попилен на 3 части

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:demo_project/main.dart';
+import 'package:demo_project/fnc/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
